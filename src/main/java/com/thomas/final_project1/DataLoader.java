256d951c80c764b5c03da3803048b7feba637872
apocalypeseed <tswlew@yahoo.com>
2018-08-08 19:53:45 -0400

    Reversion to the older version

--- src/main/java/com/thomas/final_project1/DataLoader.java
+++ src/main/java/com/thomas/final_project1/DataLoader.java
@@ -5,7 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-/*
+
 @Component
 public class DataLoader implements CommandLineRunner
 {
@@ -31,4 +31,3 @@ public class DataLoader implements CommandLineRunner
 
     }
 }
-*/
